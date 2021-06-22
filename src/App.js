e8211fa7da56e31ba0f378726ddfea7bcd3154c1
Priscila Veras <vrspriscila@gmail.com>
2021-06-22 02:34:30 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import ironlogo from "./assets/images/ironhack-logo.svg";
+import menuimg from "./assets/images/menu-top.svg";
+import backLogo from "./assets/images/react-logo.svg";
+import icon1 from "./assets/images/icon1.png";
+import icon2 from "./assets/images/icon2.png";
+import icon3 from "./assets/images/icon3.png";
+import icon4 from "./assets/images/icon4.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container1">
+        <header className="navbar">
+          <img src={ironlogo} />
+          <img src={menuimg} />
+        </header>
+        <div className="d-flex justify-content-between ">
+          <div className="text-react align-items-center  ">
+            <h1>Say hello to ReactJS</h1>
+
+            <p>
+              You will learn how to use the most popular frontend library, and
+              become a super Ninja developer.
+            </p>
+
+            <button className="btn ">Awesome!</button>
+          </div>
+          <div className="background-images align-items-center ">
+            <img src={backLogo} className="img-g" />
+            <img src={backLogo} className="img-m " />
+            <img src={backLogo} className="img-m " />
+
+            <img src={backLogo} className="img-p" />
+            <img src={backLogo} className="img-p" />
+            <img src={backLogo} className="img-p" />
+          </div>
+        </div>
+      </div>
+
+      <div className="container2">
+        <div className="row justify-content-center">
+          <div className="col md-3 text-icon">
+            <img src={icon1} />
+            <h2>Declarative</h2>
+            <p>React makes it painless to create interactive UIs.</p>
+          </div>
+          <div className="col md-3 text-icon">
+            <img src={icon2} />
+            <h2>Components</h2>
+            <p>Build encapsulated components that manage their state.</p>
+          </div>
+          <div className="col md-3 text-icon">
+            <img src={icon3} />
+            <h2>Single-Way</h2>
+            <p>A set of immutable values are passed to the component's</p>
+          </div>
+          <div className="col md-3 text-icon">
+            <img src={icon4} />
+            <h2>JSX</h2>
+            <p>Statically-typed, designed to run on modern browsers</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
